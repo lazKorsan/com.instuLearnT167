3f351b43775ecf6ff3d471ac4aac2018ba86d35f
lazKorsan <ahmeteldes2002@gmail.com>
2026-04-24 06:26:43 +0300

    duzenleme

--- src/test/java/stepdefinitions/US023CreateBundleSteps.java
+++ src/test/java/stepdefinitions/US023CreateBundleSteps.java
@@ -19,26 +19,30 @@ public class US023CreateBundleSteps {
     WebDriver driver = Hooks.getDriver();
     ExamplePage examplePage = new ExamplePage(driver);
     CreateBundlePage createBundlePage = new CreateBundlePage(driver);
-
     public static final String nextButton="//button[@id='getNextStep']";
+
+
 
 
     @Given("Instructor {string} adresi ve {string} ile sisteme giris yapar")
     public void ınstructor_adresi_ve_ile_sisteme_giris_yapar(String mail, String password) {
 
+        // todo Kullanıcı login sayfasına gider
         logger.debug("🐞 Kullanıcı login sayfasına gider");
         driver.get("https://qa.instulearn.com/login");
         logger.info("🌏 Kullanıcı login sayfasına gitti");
 
-
+        // todo Kullanıcı mail adresini giriyor
         logger.debug("🐞 Kullanici mail adresini giriyor");
         SendKeysUtils.sendByXpath(driver, "//input[@id='email']", mail);
         logger.info("✅ Kullanıcı mail adresini girdi");
 
+        // todo Kullanıcı password adresini giriyor
         logger.debug("🐞 Kullanici password adresini giriyor");
         SendKeysUtils.sendByXpath(driver, "//input[@id='password']", password);
         logger.info("✅ Kullanıcı password adresini girdi");
 
+        // todo Kullanıcı submit butonuna basıyor
         logger.debug("🐞 Kullanıcı submit butonuna tıklıyor");
         ClickUtils.clickByXpath(driver, "//button[@class='btn btn-primary btn-block mt-20']");
         logger.info("✅ Kullanıcı submit butonuna tıkladı");
@@ -49,18 +53,17 @@ public class US023CreateBundleSteps {
     @When("Kullanici dashBoard menuden newButtonuna tiklar")
     public void kullaniciDashBoardMenudenNewButtonunaTiklar() {
 
-
+        // todo dashBoardMenu ye hoover islemleri
         Actions actions = new Actions(driver);
         WebElement dashboardMenu = driver.findElement(By.xpath("(//span[@class='font-14 text-dark-blue font-weight-500'])[1]"));
         actions.moveToElement(dashboardMenu).perform();
         ReusableMethods.bekle(2);
         logger.info("✅  Kullanici dashBoard Menuye Hover yapti");
 
-        // todo dashBoard islemleri
+        // todo dashboard icinde coursesBox secim islemleri
         ClickUtils.clickByXpath(driver, "(//span[@class='font-14 text-dark-blue font-weight-500'])[2]");
         ReusableMethods.bekle(2);
         logger.info("✅ Kullanici dashboard menu altindan coursesButtona tikladi ");
-
 
         // todo newCourses islemleri
         ClickUtils.clickByXpath(driver, "//*[@class=\"mt-5 \"]");
@@ -71,10 +74,12 @@ public class US023CreateBundleSteps {
 
     @When("Kullanici newWebinar sayfasinda oldugunu dogrular")
     public void kullaniciNewWebinarSayfasindaOldugunuDogrular() {
+        // todo
         driver.get("https://qa.instulearn.com/panel/webinars/new");
         logger.info("✅ Kullanici newWebinars sayfasina gitti ");
         ReusableMethods.bekle(2);
 
+        // todo url dogrulama adimlari
         String expectedUrl="https://qa.instulearn.com/register";
         String actualUrl=driver.getCurrentUrl();
 
@@ -85,36 +90,40 @@ public class US023CreateBundleSteps {
             logger.error("Beklenen url: " + expectedUrl + "ile" + actualUrl + "farklı degerde");
         }
 
-        ReusableMethods.bekle(15);
+        ReusableMethods.bekle(8); // fixme bekleme suresi aayarlanacak
     }
 
     @When("Kullanici basicInformation sayfasi bilgilerini doldurur")
     public void kullaniciBasicInformationSayfasiBilgileriniDoldurur() {
 
+        // todo courseTypeButton islemleri
         ClickUtils.clickByXpath(driver, "//*[@id=\"webinarForm\"]/div[2]/div/div[1]/select");
         logger.info("Kullanici courseType Container alanina tikladi");
         ReusableMethods.bekle(2);
 
+        // todo courseTypeDropDown Menuden VideoCourse Type secimi islemleri
         ClickUtils.clickByXpath(driver, "//*[@id=\"webinarForm\"]/div[2]/div/div[1]/select/option[2]");
         logger.info("Kullanici courseType Container alanindan 2nci opsiyonu secti.VideoCourse Option");
         ReusableMethods.bekle(2);
 
+        // todo titleBox islemleri
         SendKeysUtils.sendByXpath(driver, "//input[@name=\"title\"]","Math-251");
         logger.info("Kullanici Title alanina Math-251 yazdi");
         ReusableMethods.bekle(2);
 
-
+        // todo ceoDescriptionsBox alani islemleri
         SendKeysUtils.sendByXpath(driver, "//input[@name=\"seo_description\"]", "Math,VideoCourse,Certificate");
         logger.info("Kullanici seo_description alanina Math,VideoCourse,Certificate yazdi");
         ReusableMethods.bekle(2);
 
-
+        // todo  SWİTCH WİNDOW İSLEMLERİNİN OLDUGU BOLUM
+        // todo thunbnail yukleme islemleri
         ClickUtils.clickByXpath(driver,"(//button[@class=\"input-group-text panel-file-manager\"])[1]");
         ClickUtils.clickByXpath(driver,"(//button[@class=\"input-group-text panel-file-manager\"])[1]");
         logger.info("Kullanici thumbnail alanina tiklandi");
         ReusableMethods.bekle(4);
 
-
+        // todo switchWindow ile ikinci pencereye gecme
         SwitchToWindow.switchToWindowByIndex(driver,1);
         logger.info("Kullanici switchToWindow ozelligi ile ikinci pencereye gecti");
 
@@ -125,17 +134,16 @@ public class US023CreateBundleSteps {
         ClickUtils.clickByXpath(driver, "//*[@class=\"fas fa-check\"]");
         logger.info("Kullanici confirmButtn a tikladi ");
 
-
+        // todo switchWindow ile ilk pencereye gecme
         SwitchToWindow.switchToWindowByIndex(driver,0);
         logger.info("Kullanici switchToWindow ozelligi ile ilk pencereye gecti");
         ReusableMethods.bekle(2);
 
-
-        // todo
+        // todo çift tiklama alternatif olarak ReusableMethods içinden cagirilabilir
         ClickUtils.clickByXpath(driver,"(//button[@class=\"input-group-text panel-file-manager\"])[2]");
         ClickUtils.clickByXpath(driver,"(//button[@class=\"input-group-text panel-file-manager\"])[2]");
 
-
+        // todo switchWindow ile ikinci pencereye gecme
         SwitchToWindow.switchToWindowByIndex(driver,1);
         logger.info("Kullanici switchToWindow ozelligi ile ikinci pencereye gecti");
 
@@ -146,17 +154,21 @@ public class US023CreateBundleSteps {
         ClickUtils.clickByXpath(driver, "//*[@class=\"fas fa-check\"]");
         logger.info("Kullanici confirmButtn a tikladi ");
 
-
+        // todo switchWindow ile ilk pencereye gecme
         SwitchToWindow.switchToWindowByIndex(driver,0);
         logger.info("Kullanici switchToWindow ozelligi ile ilk pencereye gecti");
         ReusableMethods.bekle(2);
+        // todo switchWindow islemleri bitti . Driverin o indexine donmesi gerekir tekrardan
 
+
+
+        // todo description islemleri
         String description="Limit, türev ve integralin fizikteki kullanım alanları. Anlık hız, ivme, iş, enerji, elektrik alan ve akışkanlar mekaniği problemlerinin matematiksel çözümlemesi.";
-
         SendKeysUtils.sendByXpath(driver, "//div[@role='textbox']", description);
         logger.info("Kullanici course descirption alanini doldurdu");
         ReusableMethods.bekle(2);
 
+        // todo nextButton islemleri
         ClickUtils.clickByXpath(driver, nextButton);
         ReusableMethods.bekle(3);
 
@@ -165,7 +177,7 @@ public class US023CreateBundleSteps {
     @When("Kullanici extraInformation sayfasi bilgilerini doldurur")
     public void kullaniciExtraInformationSayfasiBilgileriniDoldurur() {
 
-        // Bu satir sonra silinecek
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/2");
         ReusableMethods.bekle(4);
 
@@ -227,7 +239,7 @@ public class US023CreateBundleSteps {
     @When("Kullanici pricingPage sayfasi bilgilerini doldurur")
     public void kullaniciPricingPageSayfasiBilgileriniDoldurur() {
 
-        // bu satır sonra silinecek
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/3");
 
         // todo subscribeSwitchCheckBox islemleri
@@ -256,15 +268,12 @@ public class US023CreateBundleSteps {
 
         // https://qa.instulearn.com/panel/webinars/4130/step/4
 
-
-
-
     }
 
     @When("Kullanici contentPage sayfasi bilgilerini doldurur")
     public void kullaniciContentPageSayfasiBilgileriniDoldurur() {
 
-        // bu satır sonra sil
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/4");
 
         // todo newSectionButton islemleri
@@ -278,7 +287,6 @@ public class US023CreateBundleSteps {
         ReusableMethods.bekle(2);
         logger.info("Kullanici sectionTitleBox alanini doldurdu");
 
-
         // todo modalSavebutton islemleri
         // modalSavebutton="(//button[@class="save-chapter btn btn-sm btn-primary"])[2]";
         String modalSavebutton="(//button[@class=\"save-chapter btn btn-sm btn-primary\"])[2]";
@@ -286,7 +294,7 @@ public class US023CreateBundleSteps {
         ReusableMethods.bekle(2);
         logger.info("Kullanici modalSavebutton alanini doldurdu");
 
-        // nextStepButton
+        // todo nextStepButton islemleri
         ClickUtils.clickByXpath(driver, nextButton);
         ReusableMethods.bekle(3);
 
@@ -297,7 +305,7 @@ public class US023CreateBundleSteps {
     @When("Kullanici prerequisitesPage sayfasi bilgilerini doldurur")
     public void kullaniciPrerequisitesPageSayfasiBilgileriniDoldurur() {
 
-        // bu satır sonra sil
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/5");
 
         // todo newPrerequisitesButton islemleri
@@ -314,37 +322,28 @@ public class US023CreateBundleSteps {
 
         // todo searchBox islemleri
         String searchBox = "//input[@class='select2-search__field']";
-       // 1. Kelimeyi gönder (Static metodumuzla hem yazıyor hem vurguluyor)
         SendKeysUtils.sendByXpath(driver, searchBox, "adv");
         ReusableMethods.bekle(2);
 
-        // 2. KLİT NOKTA: Aşağı Ok tuşuna bas (Artık bu da static metodumuzun içinde)
+        // todo  burasi onemli bir ozellik. ArrowOzelligi ile dropdown menuden secme islemi
+        // KLİT NOKTA: Aşağı Ok tuşuna bas (Artık bu da static metodumuzun içinde)
         // Bu adım, açılan listenin ilk elemanını 'aktif' hale getirir.
         SendKeysUtils.pressArrowDown(driver, searchBox);
         ReusableMethods.bekle(1);
         logger.info("🔍 [Step] Arama sonuçları arasında aşağı inildi.");
 
-        // 3. ENTER ile seçimi onayla
+        // todo arrowDown sonrası KEYS_ENTER islemi
         SendKeysUtils.pressEnter(driver, searchBox);
         ReusableMethods.bekle(1);
         logger.info("✅ [Step] Seçim yapıldı ve onaylandı.");
         ReusableMethods.bekle(2);
 
-
         // todo requiredCheckBoxButton islemleri
-        // 1. XPath'i tanımlıyoruz (Statik ID yerine dinamik olanı kullandım)
         String requiredCheckBoxButton = "//*[@class=\"custom-control custom-switch\"]";
-
-        // 2. Elementi önce Selenium ile buluyoruz
         WebElement checkboxElement = driver.findElement(By.xpath(requiredCheckBoxButton));
         ReusableMethods.highLightToElement(checkboxElement);
-
-        // 4. JavaScriptExecutor ile "Arka Kapıdan" tıklama
-        // Bu yöntem, elementin üzerinde başka bir katman olsa bile doğrudan DOM üzerinden tetikler
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", checkboxElement);
-
-        // 5. Bekleme ve Log
         ReusableMethods.bekle(2);
         logger.info(" ✅ Kullanici requiredCheckBoxButton alanini JS Executor ile başarıyla doldurdu");
 
@@ -360,10 +359,10 @@ public class US023CreateBundleSteps {
     @When("Kullanici FAQpage sayfasi bilgilerini doldurur")
     public void kullaniciFAQpageSayfasiBilgileriniDoldurur() {
 
-        // bu satır sonra sil
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/6");
 
-        // todo
+        // todo newFAQButton islemleri
         String newFAQButton="//button[@id=\"webinarAddFAQ\"]";
         ClickUtils.clickByXpath(driver, newFAQButton);
         ReusableMethods.bekle(4);
@@ -384,7 +383,6 @@ public class US023CreateBundleSteps {
 
         // todo saveFAQButton islemleri
         String saveButton="//button[@class=\"js-save-faq btn btn-sm btn-primary\"]";
-
         ClickUtils.clickByXpath(driver, saveButton);
         ReusableMethods.bekle(3);
         logger.info("Kullanici saveButton alanini doldurdu");
@@ -398,7 +396,7 @@ public class US023CreateBundleSteps {
     @When("Kullanici Quiz&CertificationPage sayfasi bilgilerini doldurur")
     public void kullaniciQuizCertificationPageSayfasiBilgileriniDoldurur() {
 
-        // bu satir sonra silinecek 7 . step
+        // todo ÖNEMLİ DYNAMİK TESTE GEÇERKEN BURAYI YORUMA AL
         driver.get("https://qa.instulearn.com/panel/webinars/4130/step/7");
 
         // todo newQuizButton islemleri
@@ -445,7 +443,6 @@ public class US023CreateBundleSteps {
         logger.info("Kullanici expiryDaysBox alanini doldurdu");
 
         // todo Display questions randomly Box  islemleri javaexceture ile
-
         ClickUtils.clickByXpath(driver, "//*[@class=\"custom-control custom-switch\"]");
         ReusableMethods.bekle(2);
         logger.info("Kullanici Display questions randomly Box alanini JS Executor ile başarıyla doldurdu");
